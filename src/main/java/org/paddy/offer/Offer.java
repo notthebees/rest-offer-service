39d2f056a6aaffdc5f0ea9c8e7ce0aab15c688cb
paddy <paddy@marinus.uk>
2017-09-24 19:35:02 +0100

    Implement offer entity

--- src/main/java/org/paddy/offer/Offer.java
+++ src/main/java/org/paddy/offer/Offer.java
@@ -1,7 +1,60 @@
 package org.paddy.offer;
 
-public class Offer {
-    public Offer(String description, int price) {
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Offer {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false)
+    @NotNull
+    private String description;
+
+    @Column
+    private int price;
+
+
+    protected Offer() {
+        // for JPA only
+    }
+
+    @JsonCreator
+    public Offer(@JsonProperty("description") String description, @JsonProperty("price") int price) {
+        this.description = description;
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
     }
 }
